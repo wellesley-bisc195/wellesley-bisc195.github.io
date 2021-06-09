c0a17ee44f046a86f203236bdc8ae8078461c7f8
Kevin Bonham <kevbonham@gmail.com>
2021-06-09 10:16:44 -0400

    fix assignment 1 exports

--- assignment_repos/Assignment01/src/Assignment01.jl
+++ assignment_repos/Assignment01/src/Assignment01.jl
@@ -3,7 +3,9 @@ module Assignment01
 # assignment
 export question1,
        question2,
-       question3
+       question3,
+       question4,
+       question5
 
 include("some_variables.jl")
 include("some_functions.jl")
